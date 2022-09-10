2697271143876205d333df44d7ae377c72054f2d
Testarossa <austin.w.burrows@gmail.com>
2022-09-10 02:15:32 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+use_experimental_fxv2_oal 'yes'
+lua54        'yes'
+game         'gta5'
+
+--[[ Resource Information ]]--
+name         'ox_commands'
+version      '0.0.0'
+author       'Kallock'
+
+
+--[[ Manifest ]]--
+
+client_scripts {
+	'main.lua',
+}
+
