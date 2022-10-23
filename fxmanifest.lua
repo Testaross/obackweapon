aefe07de0d3a84f93a6fb0a6110db88c787c9333
Testarossa <austin.w.burrows@gmail.com>
2022-10-22 23:17:20 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,13 @@
 --[[ FX Information ]]--
 fx_version   'cerulean'
-use_experimental_fxv2_oal 'yes'
 lua54        'yes'
 game         'gta5'
 
 --[[ Resource Information ]]--
-name         'ox_commands'
+name         'obackweapon'
 version      '0.0.0'
 author       'Kallock'
+description   'original code by kallock, being expanded upon by testarossa'
 
 
 --[[ Manifest ]]--
